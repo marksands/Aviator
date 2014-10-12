ac3186f88b574218ddbcd6e95d86f594e2a10e96
Mark Sands <marksands07@gmail.com>
2014-10-12 16:13:00 -0500

    Silence warnings

--- AviatorTests/FakeXcode.m
+++ AviatorTests/FakeXcode.m
@@ -9,24 +9,32 @@
                                                   error:(id)error {return nil;}
 @end
 @implementation PBXReference
+- (BOOL)isGroup { return NO; }
+- (id)group { return nil; }
+- (id)container { return nil; }
 - (NSString *)name { return nil; }
 - (NSSet *)includingTargets {
     return nil;
 }
+- (void)flattenItemsIntoArray:(NSArray *)obj { }
 - (NSString *)absolutePath { return nil; }
 @end
 @implementation PBXFileReference @end
 @implementation DVTFilePath@end
 @implementation IDESourceCodeDocument
+- (NSArray *)knownFileReferences { return nil; }
 -(DVTFilePath *)filePath {
     return nil;
 }
 @end
 @implementation PBXTarget
+- (NSString *)name { return nil; }
 - (BOOL)_looksLikeUnitTestTarget { return NO; }
 @end
 
-@implementation PBXContainer@end
+@implementation PBXContainer
+- (NSString *)name { return nil; }
+@end
 @implementation PBXObject@end
 @implementation PBXContainerItem@end
 
